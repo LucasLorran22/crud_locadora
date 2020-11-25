ec3ac6fc515642390f2f1b63c94276810cca35e9
Lucas Lorran <lucaslorran2016@gmail.com>
2020-11-25 20:58:06 -0300

    Inserido arquivo de conexao com o BD

---
+++ conexao.php
@@ -0,0 +1,12 @@
+<?php
+
+try {
+    //Conexão com o banco de dados.
+    $conectar = new PDO ("mysql:host=localhost;porta=3306;dbname=pdo", "root", "");
+} catch (PDOException $e) {
+
+    //Caso ocorra algum erro na conexão com o banco, exibe a mensagem.
+    echo 'Falha ao conectar com o Banco de dados: ' . $e->getMessage();
+}
+
+?>
